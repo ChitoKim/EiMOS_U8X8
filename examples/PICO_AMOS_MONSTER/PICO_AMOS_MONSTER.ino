8083ed21ed0937c71ef8641c83a08b3271e4d4a1
Chito Kim <nekroztrap@gmail.com>
2023-04-16 00:00:31 +0900

    Changes to pico example

--- examples/PICO_AMOS_MONSTER/PICO_AMOS_MONSTER.ino
+++ examples/PICO_AMOS_MONSTER/PICO_AMOS_MONSTER.ino
@@ -3,10 +3,6 @@ int address_pin[] =
 {
   21, 20, 19, 18
 };
-// int enable_pin[]= 
-// {
-//   9, 10
-// };
 int analog_pin[] = 
 {
   A2, A2, A2, A2,
@@ -16,28 +12,40 @@ int analog_pin[] =
 };
 int button_mode[] =
 {
-  6, 7, 8, 9
+  2, 3, 4, 5
 };
-int button_honba = 10;
+int button_honba = 22;
 float RES_AMOS_MONSTER[] =
 {
   20.0f, 100.0f, 1000.0f ,1000.0f
 };
 float R_REF[] =
 {
-  2.0f, 22.0f, 22.0f, 22.0f 
+  10.0f, 10.0f, 10.0f, 10.0f
 };
 mahjongAsst_U8X8 Asst(analog_pin, RES_AMOS_MONSTER, R_REF);
 
-U8X8_SSD1306_128X64_NONAME_HW_I2C oled0(U8X8_PIN_NONE);
-U8X8_SSD1306_128X64_NONAME_HW_I2C oled1(U8X8_PIN_NONE);
-U8X8_SSD1306_128X64_NONAME_2ND_HW_I2C oled2(U8X8_PIN_NONE);
-U8X8_SSD1306_128X64_NONAME_2ND_HW_I2C oled3(U8X8_PIN_NONE);
+// Hardware I2C 
 
-// U8X8_SSD1306_128X64_NONAME_SW_I2C lcd0(SCL0, SDA0, U8X8_PIN_NONE);
-// U8X8_SSD1306_128X64_NONAME_SW_I2C lcd1(SCL1, SDA1, U8X8_PIN_NONE);
-// U8X8_SSD1306_128X64_NONAME_SW_I2C lcd2(SCL2, SDA2, U8X8_PIN_NONE);
-// U8X8_SSD1306_128X64_NONAME_SW_I2C lcd3(SCL3, SDA3, U8X8_PIN_NONE);
+// U8X8_SSD1306_128X64_NONAME_HW_I2C oled0(U8X8_PIN_NONE);
+// U8X8_SSD1306_128X64_NONAME_HW_I2C oled1(U8X8_PIN_NONE);
+// U8X8_SSD1306_128X64_NONAME_2ND_HW_I2C oled2(U8X8_PIN_NONE);
+// U8X8_SSD1306_128X64_NONAME_2ND_HW_I2C oled3(U8X8_PIN_NONE);
+
+// Software I2C
+
+const int SCL0 = 6;
+const int SDA0 = 7;
+const int SCL1 = 8;
+const int SDA1 = 9;
+const int SCL2 = 10;
+const int SDA2 = 11;
+const int SCL3 = 12;
+const int SDA3 = 13;
+U8X8_SSD1306_128X64_NONAME_SW_I2C oled0(SCL0, SDA0, U8X8_PIN_NONE);
+U8X8_SSD1306_128X64_NONAME_SW_I2C oled1(SCL1, SDA1, U8X8_PIN_NONE);
+U8X8_SSD1306_128X64_NONAME_SW_I2C oled2(SCL2, SDA2, U8X8_PIN_NONE);
+U8X8_SSD1306_128X64_NONAME_SW_I2C oled3(SCL3, SDA3, U8X8_PIN_NONE);
 
 U8X8 oled[4] = {oled0, oled1, oled2, oled3};
 int i2c_address[4] = {0x3C *2, 0x3D * 2, 0x3C *2, 0x3D * 2};
@@ -51,13 +59,11 @@ setup()
   Asst.setMesType(RES);     //choose measure type; resistance(RES) or CAP(capacitance)
   Asst.setPullType(PULLDOWN); //choose whether to pull up or down the reference resistors
                               //one of these: PULLUP, PULLDOWN, INPUT_PULLUP
-  // Asst.setMUX4051(address_pin, enable_pin); //set two 74hc4051 ics to multiplex 16 analog inputs
-                                            //3 address pins shared, and 
   Asst.setMUX4067(address_pin);
-  Asst.setModeButton(button_mode);
-  Asst.setHonbaButton(button_honba);
+  // Asst.setModeButton(button_mode);   //uncommment if you use mode buttons
+  // Asst.setHonbaButton(button_honba); //uncomment if you use honba buttons
   Asst.setDisplay(oled);
-  Asst.setI2CAddress(i2c_address);
+  // Asst.setI2CAddress(i2c_address);
   Asst.initDisplay();
 }
 void
